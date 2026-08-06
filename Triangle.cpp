867f5601258fb65a3c20e2a386cf013b2c2e1809
HiyakawaRiku <ebicha0623@gmail.com>
2026-08-07 01:29:57 +0900

    Triangleクラスの変更

--- Triangle.cpp
+++ Triangle.cpp
@@ -1,44 +1,97 @@
 #include "Triangle.h"
-#include "Model.h"
 
 void Triangle::Initialize(Vector4 vertex0, Vector4 vertex1, Vector4 vertex2)
 {
-	BaseObject::Initialize();
-
 	CreateVertexResource();
-	CreateVertexData(vertex0,vertex1,vertex2);
+	CreateVertexData(vertex0, vertex1, vertex2);
+
+	// 定数バッファの生成
+	CreateMaterialResource();
+	CreateWvpResource();
+	CreateDirectionalLight();
 }
 
 void Triangle::CreateVertexResource()
 {
-	// 実際に頂点リソースを作る
-	vertexResource = dxCommon_->CreateBufferResource(dxCommon_->GetDevice(), sizeof(VertexData) * 3);
+	vertexResource_ = dxCommon_->CreateBufferResource(dxCommon_->GetDevice(), sizeof(VertexData) * 3);
 
-	// リソースの先頭のアドレスから使う
-	vertexBufferView.BufferLocation = vertexResource->GetGPUVirtualAddress();
-	// 使用するリソースのサイズは頂点3つ分のサイズ
-	vertexBufferView.SizeInBytes = sizeof(VertexData) * 3;
-	// 1頂点あたりのサイズ
-	vertexBufferView.StrideInBytes = sizeof(VertexData);
+	vertexBufferView_.BufferLocation = vertexResource_->GetGPUVirtualAddress();
+	vertexBufferView_.SizeInBytes = sizeof(VertexData) * 3;
+	vertexBufferView_.StrideInBytes = sizeof(VertexData);
 }
 
 void Triangle::CreateVertexData(Vector4 vertex0, Vector4 vertex1, Vector4 vertex2)
 {
-	// 頂点リソースにデータを書き込む
 	VertexData* vertexData = nullptr;
-	// 書き込むためのアドレスを取得
-	vertexResource->Map(0, nullptr,
-		reinterpret_cast<void**>(&vertexData));
+	vertexResource_->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
+
 	// 左下
 	vertexData[0].position = vertex0;
 	vertexData[0].texcoord = { 0.0f, 1.0f };
-	vertexData[0].normal = { 0.0f, 0.0f,-1.0f };
+	vertexData[0].normal = { 0.0f, 0.0f, -1.0f };
 	// 上
 	vertexData[1].position = vertex1;
 	vertexData[1].texcoord = { 0.5f, 0.0f };
-	vertexData[1].normal = { 0.0f, 0.0f,-1.0f };
+	vertexData[1].normal = { 0.0f, 0.0f, -1.0f };
 	// 右下
 	vertexData[2].position = vertex2;
 	vertexData[2].texcoord = { 1.0f, 1.0f };
-	vertexData[2].normal = { 0.0f, 0.0f,-1.0f };
+	vertexData[2].normal = { 0.0f, 0.0f, -1.0f };
+}
+
+void Triangle::CreateMaterialResource()
+{
+	materialResource_ = dxCommon_->CreateBufferResource(dxCommon_->GetDevice(), sizeof(Material));
+	materialResource_->Map(0, nullptr, reinterpret_cast<void**>(&materialData_));
+	materialData_->color = { 1.0f, 1.0f, 1.0f, 1.0f };
+	materialData_->lightingType = 1; // LightType_Lambert
+	materialData_->uvTransform = MakeIdentity4x4();
+}
+
+void Triangle::CreateWvpResource()
+{
+	wvpResource_ = dxCommon_->CreateBufferResource(dxCommon_->GetDevice(), sizeof(TransformationMatrix));
+	wvpResource_->Map(0, nullptr, reinterpret_cast<void**>(&wvpData_));
+	wvpData_->WVP = MakeIdentity4x4();
+}
+
+void Triangle::CreateDirectionalLight()
+{
+	directionalLightResource_ = dxCommon_->CreateBufferResource(dxCommon_->GetDevice(), sizeof(DirectionalLight));
+	directionalLightResource_->Map(0, nullptr, reinterpret_cast<void**>(&directionalLightData_));
+	directionalLightData_->color = { 1.0f, 1.0f, 1.0f, 1.0f };
+	directionalLightData_->direction = { 0.0f, -1.0f, 0.0f };
+	directionalLightData_->intensity = 1.0f;
+}
+
+void Triangle::Draw(const Transform& transform, Camera* camera, TextureHandle textureHandle)
+{
+	// 1. 行列とマテリアルの更新
+	Transform currentTransform = transform;
+	currentTransform.UpdateMatrix();
+
+	if (wvpData_ && camera) {
+		*wvpData_ = camera->CalculateWVP(currentTransform);
+	}
+
+	if (materialData_) {
+		materialData_->color = this->color;
+		materialData_->lightingType = this->lightingType;
+	}
+
+	// 2. 描画コマンドの発行
+	auto commandList = dxCommon_->GetCommandList();
+
+	commandList->IASetVertexBuffers(0, 1, &vertexBufferView_);
+	commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	commandList->SetGraphicsRootConstantBufferView(0, materialResource_->GetGPUVirtualAddress());
+	commandList->SetGraphicsRootConstantBufferView(1, wvpResource_->GetGPUVirtualAddress());
+
+	D3D12_GPU_DESCRIPTOR_HANDLE srvHandle = TextureManager::GetInstance()->GetSrvHandleGPU(textureHandle);
+	commandList->SetGraphicsRootDescriptorTable(2, srvHandle);
+	commandList->SetGraphicsRootConstantBufferView(3, directionalLightResource_->GetGPUVirtualAddress());
+
+	// 三角形（3頂点）の描画
+	commandList->DrawInstanced(3, 1, 0, 0);
 }
