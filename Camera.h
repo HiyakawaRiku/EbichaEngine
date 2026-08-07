a48a9c86447b3226e87890bc8f5293edb069304d
HiyakawaRiku <ebicha0623@gmail.com>
2026-08-07 09:17:46 +0900

    BaseObjectの削除

--- Camera.h
+++ Camera.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "BaseObject.h"
+#include "Struct.h"
 
 class Camera
 {
---
+++ Struct.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "DirectXCommon.h"
+#include "EMath.h"
+#include "Transform.h"
+
+
+
+struct VertexData {
+	Vector4 position;
+	Vector2 texcoord;
+	Vector3 normal;
+};
+
+enum LightType {
+	LightType_None = 0,        // ライトなし
+	LightType_Lambert = 1,     // ランバート
+	LightType_HalfLambert = 2, // ハーフランバート
+};
+
+struct Material {
+	Vector4 color;
+	int32_t lightingType;
+	float padding[3];
+	Matrix4x4 uvTransform;
+};
+
+struct TransformationMatrix {
+	Matrix4x4 WVP;
+	Matrix4x4 World;
+};
+
+struct DirectionalLight {
+	Vector4 color;
+	Vector3 direction;
+	float intensity;
+};
